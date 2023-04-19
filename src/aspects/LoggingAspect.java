94d12b3b1c7d747eb57c017eb31b2a2fb5b027ab
Adrian <96118065+Filip-Adrian@users.noreply.github.com>
2023-04-19 22:04:42 +0200

    Update LoggingAspect.java
    Update

--- src/aspects/LoggingAspect.java
+++ src/aspects/LoggingAspect.java
@@ -45,16 +45,32 @@ public class LoggingAspect {
     private static class InnerInvocationHandler implements InvocationHandler {
 
         private Object target;
-
+	
+	    /* 
+	    'InnerInvocationHandler(Object target)': è il costruttore della classe 'InnerInvocationHandler' che prende in input un oggetto 'target' 
+	    di tipo 'Object' e lo assegna alla variabile di istanza 'target'.
+	    */
         private InnerInvocationHandler(Object target) {
             this.target = target;
         }
 
+	    /*
+	    Il metodo `invoke` viene chiamato ogni volta che un metodo viene chiamato su un oggetto proxy creato tramite la classe `Proxy`. 
+	    Il metodo `invoke` prende tre parametri:
+		- `proxy`: l'oggetto proxy su cui è stato chiamato il metodo;
+		- `method`: l'oggetto `Method` che rappresenta il metodo chiamato sull'oggetto proxy;
+		- `arguments`: l'array di oggetti che rappresentano gli argomenti passati al metodo chiamato sull'oggetto proxy.
+	    */
         @Override
         public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
             
             String name = method.getName();
 
+		/* 
+		Se la chiamata del metodo ha avuto successo, viene salvato il risultato del metodo nella variabile result. 
+		Viene poi chiamato il metodo log per stampare un messaggio che indica che il metodo è terminato e qual è stato il risultato. 
+		Infine, il metodo restituisce il risultato.
+		*/
             try {
                 log("In " + name + " " + Arrays.toString(arguments));
                 Object result = method.invoke(target, arguments);
