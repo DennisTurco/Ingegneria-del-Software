9fca5eab92014e08171e532285d65218bfbde9b8
Dennis Turco <57963761+DennisTurco@users.noreply.github.com>
2023-04-19 21:13:58 +0200

    Add files via upload

--- src/aspects/PersistentHandler.java
+++ src/aspects/PersistentHandler.java
@@ -3,8 +3,10 @@ package aspects;
 import java.io.IOException;
 import java.io.Serializable;
 
+// da quello che ho capito stiamo lavorando con i tipi bounded (bounded types).
+// in questo caso il tipo parametrico non e' un tipo qualsiasi, ma deve essere compatibile con la classe Serializable
 public interface PersistentHandler<T extends Serializable> {
-    public T get();
-    public void rollback() throws IOException;
-    public void commit() throws IOException;
+	public T get();
+	public void commit() throws IOException;
+	public void rollback() throws IOException;
 }
--- src/aspects/SharedAspect.java
+++ src/aspects/SharedAspect.java
@@ -5,65 +5,55 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-// classe pubblica con un metodo statico pubblico. E' una classe factory perche' non viene istanziata
+// classe factory
 
-// un oggetto condiviso puo' essere acceduto da tanti thread e deve garantire che il calcolo della risposta dei suoi messaggi venga realizzata in mutua esclusione; tutte le volte che
-// entriamo in un metodo usiamo un lock, per esempio per accedere a una stringa;
 public class SharedAspect {
-
-    // taget deve essere un ogetto di tipo interfaccia
-    public static <T> T attach(T target) {
-        if (target == null) throw new IllegalArgumentException("target == null");
-
-        // ottengo la classe (ottenendone il nome completo, es. "class java.util.LinkedList")
-        Class<?> targetClass = target.getClass();
-
-        // dobbiamo ottenere tutte le interfacce
-		// perchè il proxy dovrà avere tutte le interfacce dell'oggetto target
-        Class<?>[] targetInterfaces = targetClass.getInterfaces();
-        
-        // il class loader è quello che è stato usato per caricare la classe da cui è stato costruito target
-        // l'obbiettivo e' quello di chiamare "invoke" sull'invocation handler
-        Object proxy = Proxy.newProxyInstance(targetClass.getClassLoader(), targetInterfaces, new InnerInvocationHandler(target));
-
-        @SuppressWarnings("unchecked")
-        T result = (T) proxy;
-        return result;
-    }
-
-
-    // siccome questa è una classe che ha come unico scopo quello di offrire un metodo statico per la costruzione
-	// di questi oggetti, è una classe factory, in particolare non vogliamo che questa classe venga istanziata, e 
-	// blocchiamo la possibilità mettendo un unico costruttore vuoto e privato
+	
 	private SharedAspect() {
 		// blank
 	}
-
-
-    // INNER CLASS
-    private static class InnerInvocationHandler implements InvocationHandler {
-        private Object target;
-        private Object lock;
-        
-        private InnerInvocationHandler(Object target) {
-            this.target = target;
-            this.lock = new Object();
-        }
-
-        @Override
-        public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
-            try {
-                synchronized (lock) {
-                    // invocazione del metodo sull'oggetto target
-                    Object result = method.invoke(target, arguments);
-                    return result;
-                }
-            } catch (InvocationTargetException exception) { // lanciata da invoke
-                throw exception.getCause();
-            } catch (Throwable throwable) {
-                throw throwable;
-            }
-        }
-
-    }
+	
+	public static <T> T attach(T target) {
+		if (target == null) throw new IllegalArgumentException("target == null");
+		
+		Class<?> targetClass = target.getClass();
+		
+		Class<?>[] targetInterfaces = targetClass.getInterfaces();
+		
+		Object proxy = Proxy.newProxyInstance(targetClass.getClassLoader(), targetInterfaces, new InnerInvocationHandler(target));
+		
+		@SuppressWarnings("unchecked")
+		T result = (T) proxy;
+	
+		return result;
+	}
+	
+	
+	// INNER CLASS
+	private static class InnerInvocationHandler implements InvocationHandler {
+		
+		private Object target;
+		private Object mutex;
+		
+		private InnerInvocationHandler(Object target) {
+			this.target = target;
+			this.mutex = new Object();
+		}
+		
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			try {
+				synchronized (mutex) {
+					Object result = method.invoke(target, args);
+					return result;
+				}
+			} catch (InvocationTargetException exception) {
+				throw exception.getCause();
+			} catch (Throwable exception) {
+				throw exception;
+			}
+			
+		}
+		
+	}
 }
