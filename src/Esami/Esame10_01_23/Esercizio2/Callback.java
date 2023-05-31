5b42aa9b6119a9e141c80dbb03add22470ed8e9e
Dennis Turco <57963761+DennisTurco@users.noreply.github.com>
2023-05-31 21:41:41 +0200

    Delete src/Esami/Esame10_01_23 directory

--- src/Esami/Esame10_01_23/Esercizio2/Callback.java
+++
@@ -1,9 +0,0 @@
-package Esami.Esame10_01_23.Esercizio2;
-
-public interface Callback<T> {
-	public void onSuccess(T result);
-	
-	public default void onFailure(Throwable exception) {
-		exception.printStackTrace();
-	}
-}
